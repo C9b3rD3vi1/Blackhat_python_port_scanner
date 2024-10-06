29cc6b415fa8415e95751ea04e36ca983edf09cc
C9b3rD3vi1 <nicksonwe@gmail.com>
2024-10-06 13:42:48 +0300

    more update on port_scanner

--- port_scanner.py
+++ port_scanner.py
@@ -3,6 +3,19 @@ import socket
 import time
 import http
 import threading
+import pyfiglet
+from colorama import Fore, Style
+
+
+
+
+# ascii banner
+ascii_banner = pyfiglet.figlet_format("Cyberlock Hacker")
+print(ascii_banner)
+print("Author: @C9b3rD3vi1")
+print("Version: 1.0.0")
+print("Description: Simple python port scanner using multithreading and requests")
+print(" ")
 
 
 
@@ -13,10 +26,11 @@ def port_scanner(ip, port):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(1)
         sock.connect((ip, port))
-        print(f" {port} on {ip} is open")
+
+        print(f"{Fore.GREEN} {port} on {ip} is open")
     
     except socket.error:
-        print(f" {port} on {ip} is closed")
+        print(f"{Fore.RED} {port} on {ip} is closed")
 
 
 
@@ -24,13 +38,20 @@ def port_scanner(ip, port):
 def main_start():
 
     start_time = time.time()
+    print(f"{Fore.YELLOW} Current time: {time.ctime()}")
     ip = input("[~] Enter a target ip address :")
     port = input(" [~] Enter port to connect :")
     port_range = port.split(":")
     start_port = int(port_range[0])
     end_port = int(port_range[1])
+
+    print(" \n")
     
-    print ("Scanning")
+    
+    print (f"{Fore.BLUE} Starting Port Scanning................")
+    print(f"{Fore.BLUE} Scanning ports {start_port} to {end_port}")
+    print(f"{Fore.BLUE} Press Ctrl+C to stop scanning")
+    print(" ")
 
 
     # Create a new thread for each port to scan
